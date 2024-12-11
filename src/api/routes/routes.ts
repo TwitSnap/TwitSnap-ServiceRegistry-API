dcd901ef9bd6f4660ca88a7dc04fae4402d9810e
Guido <107871038+gmenendez0@users.noreply.github.com>
2024-12-11 20:57:01 -0300

    Update routes.ts

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.post("/v1/registry/service", serviceController.createService);
 router.get("/v1/registry/service", serviceController.getServices);
 router.get("/v1/registry/service/:id", serviceController.getService);
-router.patch("/v1/registry/service/:id/status", serviceController.changeServiceStatus);
+router.post("/v1/registry/service/:id/status", serviceController.changeServiceStatus);
 router.get("/v1/registry/service/api-key/:apiKey", serviceController.validateApiKey);
 
 
